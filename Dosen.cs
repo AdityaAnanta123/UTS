7733268b9ed94eeaea79d30659d810b2307367cb
AdityaAnanta123 <Kadek3912@students.amikom.ac.id>
2022-05-18 18:57:04 +0700

    Ujian Tengah Semester

---
+++ Dosen.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dosen
+{
+    class Dosen
+    {
+        public int id { get; set; }
+        public string nama { get; set; }
+        public int Nik { get; set; }
+        public string gender { get; set; }
+        public string course { get; set; }
+
+        public Dosen()
+        {
+            this.id = 2;
+            this.nama = "";
+            this.Nik = 21211732;
+            this.gender = "Laki-laki";
+            this.course = "";
+        }
+        public void attDosen(string name, int nik)
+        {
+            this.nama = name;
+            this.Nik = nik;
+        }
+        public void addCourse(string course)
+        {
+            this.course = course;
+        }
+        public void Tampilan()
+        {
+            Console.WriteLine("Id     = {0}", id);
+            Console.WriteLine("Nama   = {0}", nama);
+            Console.WriteLine("NIK    = {0}", Nik);
+            Console.WriteLine("Gender = {0}", gender);
+            Console.WriteLine("Course = {0}", course);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dosen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dosen dosen1 = new Dosen();
+            dosen1.attDosen("Kamarudin, M.Kom",18932423);
+            dosen1.addCourse("Pemrograman");
+            dosen1.Tampilan();
+            Console.ReadKey();
+        }
+    }
+}
